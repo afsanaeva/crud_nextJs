7d600fb2eb94c9865cb29113f852dbe82696cb3a
afsanaeva <afsanaeva05@gmail.com>
2022-09-11 17:12:19 +0600

    add mongo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports={
+  env: {
+    MONGODB_URI: "mongodb://localhost/taskCrudApp"
+  }
+}
