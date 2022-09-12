f69c59020e3c23c2be95573dbd71b2284dc88ae9
afsanaeva <afsanaeva05@gmail.com>
2022-09-12 17:15:03 +0600

    add get post method and mongo

---
+++ src/model/Task.js
@@ -0,0 +1,28 @@
+import { Schema, model,models } from "mongoose";
+
+const TaskSchema =new Schema(
+    {
+        title:{
+            type:String,
+            require:[true, "The video title is required"],
+            unique: true,
+            trim:true,
+            maxlength:[40, "title cannot be greater than 40 characters"],
+
+        },
+        
+        link:{
+            type:String,
+            require:[true, "The video link is required"],
+            trim:true,
+            maxlength:[350, "link cannot be greater than 40 characters"],
+
+        },
+        
+    },
+    {
+        timestamps:true,
+        versionKey:false
+    }
+);
+export default models.Task || model("Task",TaskSchema);
--- src/utils/moongoose.js
+++ src/utils/moongoose.js
@@ -7,8 +7,10 @@ const conn={
 export async function  dbConnect(){
     if(conn.isConnected) return;
     
-    const db = await connect(process.env.MONGO_URI);
+    const db = await connect(process.env.MONGODB_URI);
     conn.isConnected = db.connections[0].readyState;
+
+    console.log(db.connection.db.databaseName)
 }
 
 connection.on("connected",()=>{
